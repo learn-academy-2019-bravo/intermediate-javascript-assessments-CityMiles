e31bd9cca2bf141fecc715d46aa2b53f7ace5051
MIles <wanderlust.conscious@gmail.com>
2019-05-13 14:44:27 -0700

    mostly done, need help

--- intermediate-js.js
+++ intermediate-js.js
@@ -1,10 +1,16 @@
-test
-
 // Intermediate Javascript Assessments
 
 // 1. Write an arrow function that takes one argument of type array and returns the contents of that array in reverse order.
 
 var originalArray = [1, 2, 3, 4, 5, 6, 7]
+
+arrayRev = (array) => {
+	let arrNew = array.reverse()
+	return arrNew
+}
+
+console.log(arrayRev(originalArray))
+
 
 
 // 2. Given the object below, complete the console.log to find specific information:
@@ -20,23 +26,68 @@ var bicycle = {
 }
 
 // Log the type of bicycle:
-console.log()
+console.log(bicycle.type)
 
 // Log the bell:
-console.log()
+console.log(bicycle.gear[2])
 
 // Log the PSI:
-console.log()
+console.log(bicycle.wheels.specs[2])
+
 
 
 // 3. Write a function called letterCounter that logs the number of times the letter "l" appears in ourString.
 
-var ourString = "Hello Learn Students!"
+var ourString = 'Hello Learn Students!'
+
+letterCounter = (string) => {
+	let stringNew = string.match(/l/gi).length
+	return stringNew
+}
+
+console.log(letterCounter(ourString))
+
 
 
 // 4. Write a function called getFib that returns the first 10 numbers of the Fibonacci sequence in an array. [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
+getFib = (num) => {
+	fibArray = []
+	finalNum = ''
+	currentNum = -1
+	nextNum = 1
 
-//5. Write a function called oddChecker that takes in the fullArr and uses a for loop to return a new array of only odd numbers. Copy and paste your code and refactor using ES6 syntax.
+	for (let i = 0; i <= num; i++) {
+		oldNum = nextNum
+		nextNum = currentNum + nextNum
+		currentNum = oldNum
+		finalNum = finalNum + nextNum
+	}
+
+	fibArray.push(finalNum)
+	return fibArray
+}
+
+console.log(getFib(10))
+
+
+
+
+// 5. Write a function called oddChecker that takes in the fullArr and uses a for loop to return a new array of only odd numbers.
+// Copy and paste your code and refactor using ES6 syntax.
 
 var fullArr = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+
+oddChecker = (array) => {
+	numArray = []
+
+	for (let i = 0; i < array.length; i++) {
+		if ((array[i]) !== typeof 'boolean' && (array[i]) !== typeof 'string') {
+			numArray.push(array[i])
+		}
+		return numArray
+	}
+	console.log("ugh don't know why i'm stuck on this, need help!")
+}
+
+console.log(oddChecker(fullArr))
